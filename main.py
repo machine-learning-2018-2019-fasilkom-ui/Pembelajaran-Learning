6ed0c8f701afe2906d5006178d049d2ce6cf2c28
Benny William Pardede <bpardedewilliam@gmail.com>
2019-04-19 23:53:15 +0700

    Refactor 'label'

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from NNMultiClassClassifier import NNMultiClassClassifier
 import pandas as pd
 import numpy as np
 
-network = NNMultiClassClassifier(7, 3, 500, 10e-4)
 data = pd.read_csv('clean_table.csv')
 
 X = np.array(data.drop(columns=['game_conclusion']))
@@ -15,39 +14,40 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
-labels = [0] * X_train.shape[0]
-for i in range(len(labels)):
+train_labels = [0] * X_train.shape[0]
+for i in range(len(train_labels)):
     label = y_train[i]
     if label == "HOME":
-        labels[i] = 0
+        train_labels[i] = 0
     elif label == "DRAW":
-        labels[i] = 1
+        train_labels[i] = 1
     else:
-        labels[i] = 2
+        train_labels[i] = 2
         
-one_hot_labels = np.zeros((len(labels), 3))
-for i in range(len(labels)):
-    one_hot_labels[i, labels[i]] = 1
+one_hot_train = np.zeros((len(train_labels), 3))
+for i in range(len(train_labels)):
+    one_hot_train[i, train_labels[i]] = 1
 
-network.fit(X_train, one_hot_labels)
+network = NNMultiClassClassifier(7, one_hot_train.shape[1], 500, 10e-4)
+network.fit(X_train, one_hot_train)
 
 # Model testing
 X_test = np.array(X_test)
 y_test = np.array(y_test)
-labels = [0] * X_test.shape[0]
+test_labels = [0] * X_test.shape[0]
 
-for i in range(len(labels)):
+for i in range(len(test_labels)):
     label = y_test[i]
     if label == "HOME":
-        labels[i] = 0
+        test_labels[i] = 0
     elif label == "DRAW":
-        labels[i] = 1
+        test_labels[i] = 1
     else:
-        labels[i] = 2
+        test_labels[i] = 2
 
-one_hot_labels = np.zeros((len(labels), 3))
+one_hot_test = np.zeros((len(test_labels), 3))
 
-for i in range(len(labels)):
-    one_hot_labels[i, labels[i]] = 1
+for i in range(len(test_labels)):
+    one_hot_test[i, test_labels[i]] = 1
 
-print("Accuracy: ", network.score(X_test, one_hot_labels))
+print("Accuracy: ", network.score(X_test, one_hot_test))
