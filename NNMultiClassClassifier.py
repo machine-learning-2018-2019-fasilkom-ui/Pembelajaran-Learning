f43a444343415ba380575fc1f9c2bb5a675d9ffe
Benny William Pardede <bpardedewilliam@gmail.com>
2019-04-19 22:26:14 +0700

    menjadikan ANN sebuah class agar lebih modular

---
+++ NNMultiClassClassifier.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+
+class NNMultiClassClassifier:
+
+    def __init__(self, hidden_nodes, output_labels, epoch, lr):
+        self.hidden_nodes = hidden_nodes
+        self.output_labels = output_labels
+        self.epoch = epoch
+        self.lr = lr
+
+        self.wo = np.random.rand(self.hidden_nodes, self.output_labels)
+        self.bo = np.random.randn(self.output_labels)
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_der(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
+
+    def softmax(self, A):
+        expA = np.exp(A)
+        return expA / expA.sum(axis=1, keepdims=True)
+
+    def fit(self, X, one_hot_y):
+        instances = X.shape[0]
+        attributes = X.shape[1]
+
+        self.wh = np.random.rand(attributes, self.hidden_nodes)
+        self.bh = np.random.randn(self.hidden_nodes)
+
+        for i in range(self.epoch):
+            # feedforward
+
+            # Phase 1
+            zh = np.dot(X, self.wh) + self.bh
+            ah = self.sigmoid(zh)
+
+            # Phase 2
+            zo = np.dot(ah, self.wo) + self.bo
+            ao = self.softmax(zo)
+
+            # Back Propagation
+
+            # Phase 1
+
+            dcost_dzo = ao - one_hot_y
+            dzo_dwo = ah
+
+            dcost_wo = np.dot(dzo_dwo.T, dcost_dzo)
+
+            dcost_bo = dcost_dzo
+
+            # Phases 2
+
+            dzo_dah = self.wo
+            dcost_dah = np.dot(dcost_dzo, dzo_dah.T)
+            dah_dzh = self.sigmoid_der(zh)
+            dzh_dwh = X
+            dcost_wh = np.dot(dzh_dwh.T, dah_dzh * dcost_dah)
+
+            dcost_bh = dcost_dah * dah_dzh
+
+            # Update Weights ================
+
+            self.wh -= self.lr * dcost_wh
+            self.bh -= self.lr * dcost_bh.sum(axis=0)
+
+            self.wo -= self.lr * dcost_wo
+            self.bo -= self.lr * dcost_bo.sum(axis=0)
+
+            if i % 200 == 0:
+                loss = np.sum(-one_hot_y * np.log(ao))
+                print('Loss function value: ', loss)
+
+    def score(self, X, one_hot_y):
+        zh = np.dot(X, self.wh) + self.bh
+        ah = self.sigmoid(zh)
+
+        zo = np.dot(ah, self.wo) + self.bo
+        ao = self.softmax(zo)
+
+        results = np.zeros(ao.shape)
+        for i in range(ao.shape[0]):
+            idx = np.argmax(ao[i])
+            results[i][idx] = 1
+
+        correct_num = 0
+        for result, label in zip(results, one_hot_y):
+            correct = result == label
+            if correct.all():
+                correct_num += 1
+
+        print("Accuracy: ", float(correct_num) / int(results.shape[0]))
--- algoritma.py
+++ algoritma.py
@@ -1,19 +1,22 @@
+from sklearn.model_selection import train_test_split
+from NNMultiClassClassifier import NNMultiClassClassifier
+
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 
+network = NNMultiClassClassifier(7, 3, 500, 10e-4)
 data = pd.read_csv('clean_table.csv')
 
-X = np.array(data.drop(columns = ['game_conclusion']))
+X = np.array(data.drop(columns=['game_conclusion']))
 y = data['game_conclusion']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-feature_set = np.array(X_train)
-output = np.array(y_train)
-labels = [0] * feature_set.shape[0]
+X_train = np.array(X_train)
+y_train = np.array(y_train)
 
+labels = [0] * X_train.shape[0]
 for i in range(len(labels)):
-    label = output[i]
+    label = y_train[i]
     if label == "HOME":
         labels[i] = 0
     elif label == "DRAW":
@@ -22,86 +25,28 @@ for i in range(len(labels)):
         labels[i] = 2
         
 one_hot_labels = np.zeros((len(labels), 3))
-
 for i in range(len(labels)):  
     one_hot_labels[i, labels[i]] = 1
-    
-def sigmoid(x):  
-    return 1/(1+np.exp(-x))
 
-def sigmoid_der(x):  
-    return sigmoid(x) *(1-sigmoid (x))
 
-def softmax(A):  
-    expA = np.exp(A)
-    return expA / expA.sum(axis=1, keepdims=True)
+network.fit(X_train, one_hot_labels)
 
-instances = feature_set.shape[0]  
-attributes = feature_set.shape[1]  
-hidden_nodes = 7
-output_labels = 3
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+labels = [0] * X_test.shape[0]
 
-wh = np.random.rand(attributes,hidden_nodes)  
-bh = np.random.randn(hidden_nodes)
+for i in range(len(labels)):
+    label = y_test[i]
+    if label == "HOME":
+        labels[i] = 0
+    elif label == "DRAW":
+        labels[i] = 1
+    else:
+        labels[i] = 2
 
-wo = np.random.rand(hidden_nodes,output_labels)  
-bo = np.random.randn(output_labels)  
-lr = 10e-4
+one_hot_labels = np.zeros((len(labels), 3))
 
-for epoch in range(5000):  
-############# feedforward
+for i in range(len(labels)):
+    one_hot_labels[i, labels[i]] = 1
 
-    # Phase 1
-    zh = np.dot(feature_set, wh) + bh
-    ah = sigmoid(zh)
-
-    # Phase 2
-    zo = np.dot(ah, wo) + bo
-    ao = softmax(zo)
-
-########## Back Propagation
-
-########## Phase 1
-
-    dcost_dzo = ao - one_hot_labels
-    dzo_dwo = ah
-
-    dcost_wo = np.dot(dzo_dwo.T, dcost_dzo)
-
-    dcost_bo = dcost_dzo
-
-########## Phases 2
-
-    dzo_dah = wo
-    dcost_dah = np.dot(dcost_dzo , dzo_dah.T)
-    dah_dzh = sigmoid_der(zh)
-    dzh_dwh = feature_set
-    dcost_wh = np.dot(dzh_dwh.T, dah_dzh * dcost_dah)
-
-    dcost_bh = dcost_dah * dah_dzh
-
-    # Update Weights ================
-
-    wh -= lr * dcost_wh
-    bh -= lr * dcost_bh.sum(axis=0)
-
-    wo -= lr * dcost_wo
-    bo -= lr * dcost_bo.sum(axis=0)
-    
-    if epoch % 200 == 0:
-        loss = np.sum(-one_hot_labels * np.log(ao))
-        print('Loss function value: ', loss)
-        
-
-results = np.zeros(ao.shape)
-for i in range(ao.shape[0]):
-    idx = np.argmax(ao[i])
-    results[i][idx] = 1
-    
-correct_num = 0
-for result,label in zip(results,one_hot_labels):
-    correct = result == label
-    if correct.all():
-        correct_num += 1
-        
-print("accuracy: ", float(correct_num)/int(results.shape[0]))
+network.score(X_test, one_hot_labels)
